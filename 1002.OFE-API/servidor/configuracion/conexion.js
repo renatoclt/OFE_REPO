ac7f705dc75ddf0ac815c45b46639efeb309585e
DESKTOP-0OJIEF0\roycer <roycercordova@gmail.com>
2017-11-23 10:47:31 -0500

    OFE-0000-0101 subiendo archivos de configuracion

--- 1002.OFE-API/servidor/configuracion/conexion.js
+++ 1002.OFE-API/servidor/configuracion/conexion.js
@@ -2,7 +2,8 @@ var knex = require("knex")({
   client: "sqlite3",
   connection: {
     filename: "../1002.OFE-DATABASE/dbPrueba.sqlite"
-  }
+  },
+  useNullAsDefault: false
 });
 
 module.exports = knex;
