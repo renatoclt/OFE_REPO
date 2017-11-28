11dc98c0278bdb298eaf0be57a62006768f03fdf
Renato Cohaila La Torre <renatoclt@gmail.com>
2017-11-28 13:04:42 -0500

    instalacion datos para instalar

--- 1002.OFE-API/servidor/configuracion/conexion.js
+++ 1002.OFE-API/servidor/configuracion/conexion.js
@@ -1,7 +1,7 @@
 var knex = require("knex")({
   client: "sqlite3",
   connection: {
-    filename: "../1002.OFE-DATABASE/dbPrueba.sqlite"
+    filename: "./dbPrueba.sqlite"
   },
   useNullAsDefault: false
 });
