7805c914d444dcc810fbef067b40b30940dc45c9
DESKTOP-0OJIEF0\roycer <roycercordova@gmail.com>
2017-12-04 16:21:05 -0500

    OFE-0000-0101
    agregando archivos de configuracion

--- 1002.OFE-API/servidor/rutas/rutasModulos.js
+++ 1002.OFE-API/servidor/rutas/rutasModulos.js
@@ -5,12 +5,12 @@ nav = require('./rutas_const')['nav'];
  * modulos
  */
 prueba = require('./rutasControladoresPrueba');
-producto = require('./rutasControladoresProducto');
+//producto = require('./rutasControladoresProducto');
 
 /**
  * rutas de los modulos
  */
 router.use(nav.ruta, prueba);
-router.use(nav.ruta, producto);
+//router.use(nav.ruta, producto);
 
 module.exports = router;
