f83cd0028c8708ef60e3f0cddb88943445c1b6ea
MIGUEL\miguel <miguelangel22hc@gmail.com>
2017-12-19 10:51:33 -0500

    OFE-0214-0102
    correccion ruta controladeres entidad

--- 1002.OFE-API/servidor/rutas/rutasControladoresEntidad.js
+++ 1002.OFE-API/servidor/rutas/rutasControladoresEntidad.js
@@ -2,7 +2,7 @@ var navC = nav.hijos['entidades'];
 
 var controladoresEntidad = function (ruta) {
 
-    juridico = require('../controladores/juridico/controladorJuridico')(ruta.concat(navC.hijos['juridico'].ruta),navC.hijos['juridico'].ruta);
+    juridico = require('../controladores/orgcmd/controladorJuridico')(ruta.concat(navC.hijos['juridico'].ruta),navC.hijos['juridico'].ruta);
     //juridico = require('../controladores/juridico/controladorJuridico')(ruta.concat(navC.hijos['juridico'].ruta), navC.hijos['juridico'].ruta);
     //juridico = require('../controladores/juridico/controladorJuridico')('/entidad/juridico', '/juridico');
 
