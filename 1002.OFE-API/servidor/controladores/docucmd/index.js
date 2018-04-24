f8a0ba0e6defa0789577580658699a69be7e2d53
gcampana <gacampanaq@gmail.com>
2018-04-24 02:39:23 -0500

    pdf

--- 1002.OFE-API/servidor/controladores/docucmd/index.js
+++ 1002.OFE-API/servidor/controladores/docucmd/index.js
@@ -141,8 +141,8 @@ module = (function () {
                 comprobante.igv = comprobanteJson.igv;
                 break;
             case '01':
-                idComprobante = comprobanteJson.idTipoComprobante;
-                // sInputFile = 'servidor/utilitarios/plantillasPdf/factura.xml';
+                idComprobante = Number(comprobanteJson.idTipoComprobante).toString();
+                sInputFile = 'servidor/utilitarios/plantillasPdf/factura.xml';
                 comprobante.orientacion = 'portrait';
                 comprobante.tipoComprobante = 'FACTURA ELECTRÓNICA';
                 comprobante.serie = comprobanteJson.vcSerie;
@@ -197,11 +197,11 @@ module = (function () {
                 comprobante.totalAnticipo = Number(comprobanteJson.totalAnticipos).toFixed(2);
                 comprobante.otrosTributos = Number(comprobanteJson.otrosTributos).toFixed(2);
                 comprobante.totalOtrosCargos = '0.00'; 
-                comprobante.totalComprobante = Number(comprobanteJson.totalComprobante).toFixed(2);
+                comprobante.montoPagado = Number(comprobanteJson.totalComprobante).toFixed(2);
                 break;
             case '03':
                 // sInputFile = 'servidor/utilitarios/plantillasPdf/factura.xml';
-                idComprobante = comprobanteJson.idTipoComprobante;
+                idComprobante = Number(comprobanteJson.idTipoComprobante).toString();
                 comprobante.orientacion = 'portrait';
                 comprobante.tipoComprobante = 'BOLETA ELECTRÓNICA';
                 comprobante.serie = comprobanteJson.vcSerie;
@@ -253,12 +253,14 @@ module = (function () {
                 comprobante.totalAnticipo = Number(comprobanteJson.totalAnticipos).toFixed(2);
                 comprobante.otrosTributos = Number(comprobanteJson.otrosTributos).toFixed(2);
                 comprobante.totalOtrosCargos = '0.00'; 
-                comprobante.totalComprobante = Number(comprobanteJson.totalComprobante).toFixed(2);
+                comprobante.montoPagado = Number(comprobanteJson.totalComprobante).toFixed(2);
                 break;
         }
-        
+
         let plantillaBuffer;
-        plantillaBuffer = await buscarPlantilla(idEntidad, idComprobante);
+        //  idEntidad HACODEADO CON 4
+        //  IdEntidad se carga con DocmentoEntidad Proveedor, en percepcion no devuelve
+        plantillaBuffer = await buscarPlantilla(4, idComprobante);
 
         var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
         var plantillaDecoded = Base64.decode(plantillaBuffer.dataValues.plantillaPdf); // decode the string
@@ -268,8 +270,8 @@ module = (function () {
         comprobante.logoEbiz = logoEbiz;
         
         p = new x2j.Parser();
-        // sXMLData = fs.readFileSync(sInputFile, 'utf8');
-        sXMLData = plantillaDecoded;
+        sXMLData = fs.readFileSync(sInputFile, 'utf8');
+        // sXMLData = plantillaDecoded;
     }
     function getPorcentajeOfString(cadena) {
         var posicionInicial = cadena.indexOf('(') + 1;
